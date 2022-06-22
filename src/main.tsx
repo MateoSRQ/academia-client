040b14a69e2932b14759cbe5dcdc12383317f7dd
msanroman <sanromanxp@gmail.com>
2022-06-22 11:27:06 -0500

    V2

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './pages/app'
+import Login from './pages/login'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/app" element={<App />} />
+              <Route path="/login" element={<Login />} />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,59 @@
+import { useState, useRef, useEffect } from 'react'
+import {Drawer, Button, Form, Input, Checkbox} from 'antd';
+import style from './index.module.css'
+import { useNavigate } from "react-router-dom";
+import 'animate.css';
+
+
+import 'antd/dist/antd.css'
+
+function Component() {
+    let navigate = useNavigate();
+    const handleOnFinish = (e: any) => {
+        if (e.username == 'user' && e.password == 'password') {
+            navigate("../app", { replace: true });
+        }
+        else {
+            console.log('NOK')
+        }
+    }
+
+
+    return (
+
+
+        <div className={style.component}>
+            <div className={style.container + ' ' + 'animate__animated animate__headShake'}>
+                <Form
+                    name="login"
+                    layout="vertical"
+                    onFinish={handleOnFinish}
+                >
+                    <Form.Item
+                        label="Nombre de usuario"
+                        name="username"
+                        //rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Contraseña"
+                        name="password"
+                        //rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" block>
+                            ENVIAR
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    )
+}
+
+export default Component;
